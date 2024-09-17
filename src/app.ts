7096faac155db45092d30f66a3deb1e823bf0335
vasilexa13 <vasilexa@gmail.com>
2024-09-18 00:14:05 +0300

    fix some endpoints

--- src/app.ts
+++ src/app.ts
@@ -67,15 +67,19 @@ app.post('/videos',(req, res) => {
         publicationDate: futureDate,
         availableResolutions : req.body.availableResolutions
     }
-
     db.videos.push(newVideo)
     res.status(201).json(newVideo)
-
 })
 app.put('/videos/:id', (req, res) => {
     const ID = +req.params.id;
     let findVideo  = db.videos.find((video) => video.id === ID)
     console.log(findVideo)
+    if (typeof (req.body.title)!='string'|| ((req.body.title.length<1)||(req.body.title.length>40))){
+        res.sendStatus(400)
+    }
+    if (typeof (req.body.author)!='string'|| ((req.body.author.length<1)||(req.body.author.length>20))){
+        res.sendStatus(400)
+    }
     if (!checkAvailableResolution(req.body.availableResolutions)){
         res.sendStatus(400)
     }
